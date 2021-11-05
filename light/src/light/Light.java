ce6e4e40365300d3088af90027d49cb122732276
miniwon1212 <hw991227@gmail.com>
2021-11-05 21:03:57 +0900

    어서와 자바는 처음이지 p.65

--- light/src/light/Light.java
+++ light/src/light/Light.java
@@ -1,5 +1,5 @@
 package light;
-// 어서와 자바는 처음이지 예제 문제 light.java p.65
+
 public class Light {
 	public static void main(String arg[]) {
 		long lightspeed;
@@ -7,7 +7,7 @@ public class Light {
 		
 		lightspeed = 300000;
 		distance = lightspeed * 365L * 24 * 60 * 60;
-
+		
 		System.out.println("빛이 1년 동안 가는 거리: " + distance + "km.");
 	}
 }
