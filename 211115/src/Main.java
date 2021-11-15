8fed93a416a5524ffc41555a108065be929dfe06
miniwon1212 <hw991227@gmail.com>
2021-11-15 19:03:41 +0900

    2021.11.15 백준 2588번 문제

--- 211115/src/Main.java
+++ 211115/src/Main.java
@@ -17,14 +17,27 @@ public class Main {
 		System.out.println(A / B);
 		System.out.println(A % B);
 		*/
-		
+		/* 10430번 문제
 		int A = scan.nextInt();
 		int B = scan.nextInt();
 		int C = scan.nextInt();
+		scan.close();
 		
 		System.out.println((A+B)%C);
 		System.out.println(((A%C) + (B%C))%C);
 		System.out.println((A*B)%C);
 		System.out.println(((A%C)*(B%C))%C);
+		*/
+		
+		/* 2588번 문제*/
+		
+		int n1 = scan.nextInt();
+		int n2 = scan.nextInt();
+		scan.close();
+		
+		System.out.println(n1*(n2%10));
+		System.out.println(n1*((n2%100)/10));
+		System.out.println(n1*(n2/100));
+		System.out.println(n1*n2);
 	}
 }
