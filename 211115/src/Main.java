73894f5144f79fa0f3cedd743b868c0f5dc83bd2
miniwon1212 <hw991227@gmail.com>
2021-11-15 16:58:41 +0900

    2021.11.15 백준10869번

---
+++ 211115/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+// 10869¹ø ¹®Á¦
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int A = scan.nextInt();
+		int B = scan.nextInt();
+		scan.close();
+		
+		System.out.println(A + B);
+		System.out.println(A - B);
+		System.out.println(A * B);
+		System.out.println(A / B);
+		System.out.println(A % B);
+	}
+}
