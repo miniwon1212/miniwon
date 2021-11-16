309f3c83a299d3a7437742e08da0c5161616d4b3
miniwon1212 <hw991227@gmail.com>
2021-11-16 22:11:05 +0900

    2021.11.16 백준 2884번 연수가 도와줌

--- 211116/src/Main.java
+++ 211116/src/Main.java
@@ -1,21 +1,21 @@
-import java.util.Scanner;
-// 14681¹ø
+import java.util.*;
+
+// 2884¹ø
 public class Main {
 	public static void main(String[] args) {
-		 Scanner scan = new Scanner(System.in);
-		 
-		 int x = scan.nextInt();
-		 int y = scan.nextInt();
-		 scan.close();
-		 
-		 if(x >= 0 && y >= 0) {
-			 System.out.println(1);
-		 }else if(x <= 0 && y >= 0) {
-			 System.out.println(2);
-		 }else if(x <= 0 && y <= 0) {
-			 System.out.println(3);
-		 }else if (x >= 0 && y <= 0) {
-			 System.out.println(4);
-		 }
+		Scanner scan = new Scanner(System.in);
+
+		int H = scan.nextInt(); // 0 <= H <= 23
+		int M = scan.nextInt(); // 0 <= M <= 59
+		scan.close();
+		
+		if (M < 45) {	// 0 ~ 44
+			if(H == 0) H = 24;
+			H -= 1;
+			M = M + 60 - 45;
+		} else { // 45 ~ 59
+			M -= 45;
+		}
+		System.out.println(H + " " + M);
 	}
 }
