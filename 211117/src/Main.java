45b2009746e7224f91baeb60dbc5c66e2c3f3134
miniwon1212 <hw991227@gmail.com>
2021-11-18 00:05:45 +0900

    2021.11.17 백준 10871번

--- 211117/src/Main.java
+++ 211117/src/Main.java
@@ -1,20 +1,20 @@
 import java.util.*;
-
 public class Main {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 
 		int N = scan.nextInt();
-		scan.close();
+		int X = scan.nextInt();
 
-		for (int i = 1; i <= N; i++) {
-			for(int b = N-1; b-i+1>0;b--) {
-			System.out.printf(" ");
-			}
-			for (int j = 0; j < i; j++) {
-				System.out.printf("*");
-			}
-			System.out.printf("\n");
+		
+			
+			for(int i =0; i < N; i++) {
+			
+			int A = scan.nextInt();
+
+			if (A < X)
+				System.out.printf("%d ",A);
 		}
+		scan.close();
 	}
 }
